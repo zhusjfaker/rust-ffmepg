b7a4d7725b4737d24fe926c384458f7636a3a556
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2021-07-01 16:15:36 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate libc;
+extern crate ffmpeg_dev;
+
+// use ffmpeg_dev::sys::AVFormatContext;
+
+extern {
+  fn double_input(input: libc::c_int) -> libc::c_int;
+}
+
 fn main() {
-    println!("Hello, world!");
+  let input = 4;
+  let output = unsafe { double_input(input) };
+  println!("{} * 2 = {}", input, output);
+ 
+  // let afc: AVFormatContext;
 }
