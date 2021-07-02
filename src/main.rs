eeec668adc7d5bda405b3850a36aaf3392358725
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2021-07-02 17:54:09 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-extern crate libc;
 extern crate ffmpeg_dev;
+extern crate libc;
 
-// use ffmpeg_dev::sys::AVFormatContext;
+use ffmpeg_dev::sys::avformat_open_input;
+use ffmpeg_dev::sys::AVFormatContext;
+use std::ffi::CString;
+use std::ptr::null_mut;
 
-extern {
+extern "C" {
   fn double_input(input: libc::c_int) -> libc::c_int;
 }
 
@@ -11,6 +14,12 @@ fn main() {
   let input = 4;
   let output = unsafe { double_input(input) };
   println!("{} * 2 = {}", input, output);
- 
-  // let afc: AVFormatContext;
+
+  let path = "/Users/zhushijie/Downloads/m3u8-demo/a.mp4";
+  let c_path = CString::new(path).unwrap().as_ptr();
+
+  let ifmt_ctx: *mut *mut AVFormatContext = null_mut();
+  let res = unsafe { avformat_open_input(ifmt_ctx, c_path, null_mut(), null_mut()) };
+
+  println!("res is {}", res);
 }
