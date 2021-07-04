c3a790814fb3c5bc8998dfb793b03b8b5ae9b652
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2021-07-04 18:56:29 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,18 @@ fn main() {
   let output = unsafe { double_input(input) };
   println!("{} * 2 = {}", input, output);
 
-  let path = "/Users/zhushijie/Downloads/m3u8-demo/a.mp4";
-  let c_path = CString::new(path).unwrap().as_ptr();
+  let path = "/Users/zhushijie/Desktop/m3u8-demo/b.mp4";
+  let c_path = CString::new(path).unwrap().as_ptr() as *const c_char;;
 
   let ifmt_ctx: *mut *mut AVFormatContext = null_mut();
+  // let mut ifmt_ctx: *mut AVFormatContext = std::ptr::null_mut();
   let res = unsafe { avformat_open_input(ifmt_ctx, c_path, null_mut(), null_mut()) };
 
+  if res < 0 {
+    println!("找不到视频文件");
+  } else {
+    println!("视频打开成功");
+  }
   let durtime = unsafe { **ifmt_ctx };
   println!("res is {},time is {}", res, durtime.duration);
 }
