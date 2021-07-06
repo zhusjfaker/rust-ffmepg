8de75333cec426840976699c06a0c7d1952b6d97
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2021-07-06 14:34:06 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-extern crate ffmpeg;
+extern crate ffmpeg_dev;
 extern crate libc;
 
-use ffmpeg::sys::avformat_open_input;
-use ffmpeg::sys::AVFormatContext;
+use ffmpeg_dev::sys;
+use ffmpeg_dev::sys::av_register_all;
+use ffmpeg_dev::sys::avformat_open_input;
+use ffmpeg_dev::sys::AVCodecParameters;
+use ffmpeg_dev::sys::AVFormatContext;
+use ffmpeg_dev::sys::AVMediaType_AVMEDIA_TYPE_VIDEO;
+use ffmpeg_dev::sys::AV_TIME_BASE;
 use std::ffi::CString;
 use std::ptr::null_mut;
+use std::slice::from_raw_parts;
 
 extern "C" {
   fn double_input(input: libc::c_int) -> libc::c_int;
@@ -24,7 +30,7 @@ fn main() {
   };
 
   unsafe {
-    ffmpeg::sys::av_register_all();
+    av_register_all();
     let mut ifmt_ctx: *mut AVFormatContext = null_mut();
     let code = avformat_open_input(&mut ifmt_ctx, c_path, null_mut(), null_mut());
     if code < 0 {
@@ -33,19 +39,26 @@ fn main() {
       println!("视频打开成功");
     }
     let value = (*ifmt_ctx).duration;
-    let trade = ffmpeg::sys::AV_TIME_BASE as i64;
+    let trade = AV_TIME_BASE as i64;
     let durtime = value / trade;
     let ctx = *ifmt_ctx;
     println!("res is {},time is {}", code, durtime);
     println!("stream is {}", ctx.nb_streams);
 
-    let mut i = 0;
-    loop {
-      if i > (ctx.nb_streams - 1) {
-        break;
-      } else {
-        let mut codec_ctx = ctx.streams[i].codecpar;
-        i = i + 1;
+    let stream_count = ctx.nb_streams;
+    let streams = from_raw_parts((*ifmt_ctx).streams, stream_count as usize)
+      .iter()
+      .map(|x| (*x).as_ref().expect("not null"))
+      .collect::<Vec<&sys::AVStream>>();
+
+    for (index, stream_ptr) in streams.iter().enumerate() {
+      let acc: *mut AVCodecParameters = stream_ptr.codecpar;
+      println!("codec_type is {},index is {}", (*acc).codec_type, index);
+      if (*acc).codec_type == AVMediaType_AVMEDIA_TYPE_VIDEO {
+        let codec: *mut sys::AVCodec = sys::avcodec_find_decoder((*acc).codec_id);
+        if codec == null_mut() {
+          println!("没有该类型的解码器!")
+        }
       }
     }
   }
